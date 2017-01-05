ef29f53012b38dd385b6b74d3fe035de81513e1d
QuanPV <quanpv@ows.vn>
2017-01-05 13:45:09 +0700

    a

---
+++ app/src/main/java/com/clipsub/csmanga/data/networks/NetworkService.java
@@ -0,0 +1,14 @@
+package com.clipsub.csmanga.data.networks;
+
+import com.squareup.okhttp.CacheControl;
+import com.squareup.okhttp.Headers;
+import com.squareup.okhttp.Response;
+
+import rx.Observable;
+
+public interface NetworkService {
+
+    public Observable<Response> getResponse(String url, CacheControl cacheControl, Headers headers);
+
+    public Observable<String> mapResponseToString(Response response);
+}
