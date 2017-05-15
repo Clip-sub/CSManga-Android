27ac28b5314c3125efffc36ee08e23e3a8ddc76f
Doko-Demo-Doa <doraemonfanclub@gmail.com>
2017-05-15 15:17:16 +0700

    Minor fix

--- app/src/main/kotlin/com/clipsub/csmanga/MainActivity.kt
+++ app/src/main/kotlin/com/clipsub/csmanga/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-  @Override
-  protected fun onCreate(savedInstanceState: Bundle) {
+  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
   }
 }
