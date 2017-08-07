f355efd7e4d88b606f26344a079fc35401bb4d8c
Quan Pham <doraemonfanclub@gmail.com>
2017-08-07 15:43:48 +0700

    Minor fix

---
+++ app/src/main/kotlin/com/clipsub/csmanga/ui/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.clipsub.csmanga.ui
+
+import android.support.v7.app.AppCompatActivity
+
+class BaseActivity : AppCompatActivity() {
+}
--- app/src/main/kotlin/com/clipsub/csmanga/ui/MainActivity.kt
+++ app/src/main/kotlin/com/clipsub/csmanga/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import com.clipsub.csmanga.R
 
 class MainActivity : AppCompatActivity() {
+  private val IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/"
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
