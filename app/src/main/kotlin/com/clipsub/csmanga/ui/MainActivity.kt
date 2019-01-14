2e16444713c48e83cef3315fa4e256ab58df0fdd
Quan Pham <doraemonfanclub@gmail.com>
2019-01-14 22:24:04 +0700

    Removed unused var

--- app/src/main/kotlin/com/clipsub/csmanga/ui/MainActivity.kt
+++ app/src/main/kotlin/com/clipsub/csmanga/ui/MainActivity.kt
@@ -2,20 +2,18 @@ package com.clipsub.csmanga.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.clipsub.csmanga.R
 
 class MainActivity : AppCompatActivity() {
-  private val IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/"
-  private lateinit var button : Button
+    private lateinit var button: Button
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    button = findViewById(R.id.btn_test)
-    button.setOnClickListener { v -> Toast.makeText(this, "Test Test", Toast.LENGTH_LONG).show() }
-  }
+        button = findViewById(R.id.btn_test)
+        button.setOnClickListener { v -> Toast.makeText(this, "Test Test", Toast.LENGTH_LONG).show() }
+    }
 }
