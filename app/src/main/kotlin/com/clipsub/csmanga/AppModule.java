e8e829e0b750a629a51ebf2751aeb090c88c7cb3
Quan Pham <doraemonfanclub@gmail.com>
2019-01-14 22:37:36 +0700

    Added AppModule

---
+++ app/src/main/kotlin/com/clipsub/csmanga/AppModule.java
@@ -0,0 +1,22 @@
+package com.clipsub.csmanga;
+
+import android.content.Context;
+import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Singleton;
+
+@Module
+public class AppModule {
+    private Context context;
+
+    public AppModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    @Singleton
+    public Context provideContext() {
+        return context;
+    }
+}
